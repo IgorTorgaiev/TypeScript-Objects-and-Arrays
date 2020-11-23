78f14aca76aacd57230ee81df0bd0fc85d13e98c
Igor Torgaiev <Itorgaiev@gmail.com>
2020-11-23 20:48:29 +0100

    TypeScript-Objects-and-Arrays

---
+++ sandbox.js
@@ -0,0 +1,17 @@
+// Objects and Arrays
+var topic = 'Topic: Objects and Array';
+console.log(topic);
+// Objects
+var person = {
+    name: "Igor",
+    job: "Web-Developer",
+    single: true,
+    interestedIn: "women",
+    age: 29,
+    hasBrother: false
+};
+console.log(person);
+// Arrays
+var cars = ['BMW', 'Infiniti', 'Toyota', 'Mercedes Benz'];
+console.log(cars);
+cars[0] = 'Mazda';
---
+++ sandbox.ts
@@ -0,0 +1,24 @@
+// Objects and Arrays
+
+let topic = 'Topic: Objects and Array';
+console.log(topic);
+
+// Objects
+
+let person = {
+    name: "Igor",
+    job: "Web-Developer",
+    single: true,
+    interestedIn: "women",
+    age: 29,
+    hasBrother: false
+};
+
+console.log(person);
+
+// Arrays
+
+let cars = ['BMW', 'Infiniti', 'Toyota', 'Mercedes Benz'];
+console.log(cars);
+
+cars[0] = 'Mazda';
